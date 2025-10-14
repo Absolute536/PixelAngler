d7cf8fd9aa66f94ec973d1fc6072a7a2e83fcedc
Wei Le Koh <weile.koh1286@gmail.com>
2025-10-14 17:53:32 +0800

    Initial implementation of fishing bobber's projectile motion. Include responsiveness based on facing direction next

--- scripts/Bobber.cs
+++ scripts/Bobber.cs
@@ -3,20 +3,45 @@ using System;
 
 public partial class Bobber : Sprite2D
 {
-	public Vector2 Velocity = Vector2.Zero;
-	public int InitialVelocityMagnitude = 200; // let's try 20 first
-	public float LaunchAngle = Mathf.DegToRad(0); // Launch angle is 0 because it starts horizonally from the starting point (only cos 0 = 1)
+	[Export] public Vector2 EndPoint;
 
-	public const float TimeLimit = 1.5f; // 1 second from start point to end point?
-	private double TimeTaken = 0;
-	private Vector2 startPoint = new Vector2(0, -32); // Not really (start point is 0, 0)
-	private Vector2 endPoint = new Vector2(100, 100);
-	private int range = 0;
+	// OK. I know what's going on. Because of the difference in coordinate system (y axis positive on the usual is negative here)
+	// So, instead of doing this hacky thing, we just need to negate the Endpoint y?
+	// Nope, we need to do this
+	[Export]
+	public float LaunchAngle
+	{
+		set
+		{
+			if (value != 0)
+				_launchAngle = -Mathf.DegToRad(value); // Negate if zero
+			else
+				_launchAngle = value;
+		}
+		get
+		{
+			return _launchAngle;
+		}
+
+	}
+	private float _launchAngle = 0; // Launch angle is 0 because it starts horizonally from the starting point (only cos 0 = 1)
+	private double initialVelocity;
+	private double gravity;
+	public const double TimeLimit = 0.3; // At 0.5 second, must reach the specified end point
+	private double TimeElapsed = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		// Simplified version because launch angle is zero
+		// initialVelocity = EndPoint.X / TimeLimit;
+		// gravity = -2 * EndPoint.Y / (TimeLimit * TimeLimit);
+		GD.Print(Mathf.Cos(_launchAngle));
+		GD.Print(Mathf.Sin(_launchAngle));
+		// Actual formula
+		initialVelocity = EndPoint.X / TimeLimit * (1 / Mathf.Cos(_launchAngle));
+		gravity = 2 * (initialVelocity * TimeLimit * Mathf.Sin(_launchAngle) - EndPoint.Y) / (TimeLimit * TimeLimit);
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -35,38 +60,31 @@ public partial class Bobber : Sprite2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		TimeTaken += delta;
+		TimeElapsed += delta; // Sample based on delta
 		// if (Position.X < endPoint.X && Position.Y < endPoint.Y)
-		if (TimeTaken < TimeLimit)
+		if (Position != EndPoint) // actually, it probably won't cuz delta is 1 /60 = 0.0166 (won't coincide with the time limit)
 		{
-			Vector2 currentPosition = Vector2.Zero;
-			float displacementX = (float)CalculateDisplacementX(TimeTaken);
-			float displacementY = (float)CalculateDisplacementY(TimeTaken);
+			Vector2 positionDuringMotion = Vector2.Zero;
+			float displacementX = (float)CalculateDisplacementX(TimeElapsed);
+			float displacementY = (float)CalculateDisplacementY(TimeElapsed);
 			
-			currentPosition.X = displacementX;
-			currentPosition.Y = Mathf.Tan(LaunchAngle) * displacementX - (-100 / (2 * InitialVelocityMagnitude * InitialVelocityMagnitude * Mathf.Cos(LaunchAngle) * Mathf.Cos(LaunchAngle)) * displacementX * displacementX);
-			// currentPosition.Y = displacementY;
+			positionDuringMotion.X = displacementX;
+			// positionDuringMotion.Y = Mathf.Tan(LaunchAngle) * displacementX - (-100 / (2 * InitialVelocityMagnitude * InitialVelocityMagnitude * Mathf.Cos(LaunchAngle) * Mathf.Cos(LaunchAngle)) * displacementX * displacementX);
+			positionDuringMotion.Y = displacementY;
 
-			Position = currentPosition;
+			Position = positionDuringMotion;
 			GD.Print(Position);
         }
 	}
 
-	public void SetPoints(Vector2 start, Vector2 end)
-	{
-		startPoint = start;
-		endPoint = end;
-
-	}
-
 	public double CalculateDisplacementX(double time)
 	{
-		return InitialVelocityMagnitude * time * Mathf.Cos(LaunchAngle);
+		return initialVelocity * time * Mathf.Cos(_launchAngle);
 	}
 
 	public double CalculateDisplacementY(double time)
 	{
-		return InitialVelocityMagnitude * time * Mathf.Sin(LaunchAngle) - (0.5 * -100 * time * time);
+		return initialVelocity * time * Mathf.Sin(_launchAngle) - (0.5 * gravity * time * time);
 	}
 	// Ok I think this works, just need more fine tuning to decide the gravity vector and cleaning up
 	// Also need to implement signals
